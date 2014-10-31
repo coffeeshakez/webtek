994367b046ea89296b26f4d7579f465c8a30e0ef
coff33sh4k3 <davbakke@gmail.com>
2014-10-31 09:38:36 +0100

    Added all stufs

---
+++ script.js
@@ -0,0 +1,13 @@
+var menu;
+
+document.addEventListener("load", function(){
+	menu = document.getElementById("dropdown");
+	menu.addEventListener("click", drop);
+})
+
+function drop(){
+	console.log("hei");
+
+};
+
+
